855a807a5e9768946f43901c9054c94c8418f268
qsus <106612195+qsus@users.noreply.github.com>
2024-03-30 16:03:44 +0100

    php BasicAuth

--- authenticate.php
+++ authenticate.php
@@ -1,7 +1,18 @@
 <?php
-	# check if the user is logged in
-	
+	if (!isset($_SERVER['PHP_AUTH_USER'])) { // no credentials (first connection)
+		header('HTTP/1.0 401 Unauthorized');
+		header('WWW-Authenticate: Basic realm="Notes"');
+		exit;
+	}
 
-	# if the user is not logged in, return a 404 error
-	header("HTTP/1.0 404 Not Found");
+	$user = $_SERVER['PHP_AUTH_USER'];
+	$pass = $_SERVER['PHP_AUTH_PW'];
+	
+	if ($user != "admin" || $pass != "admin") { // wrong credentials
+		header('HTTP/1.0 401 Unauthorized');
+		header('WWW-Authenticate: Basic realm="Notes"');
+		exit;
+	}
+	
+	// correct credentials
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+	require_once "authenticate.php";
 	require_once "fileLoader.php";
 	$uploads = uploadsList();
 	require "template.php";
