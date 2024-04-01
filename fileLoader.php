241b550ef41c8a4dd5cb3afd4ff2727919d38a77
qsus <106612195+qsus@users.noreply.github.com>
2024-04-01 12:53:17 +0200

    add auth to some files

--- fileLoader.php
+++ fileLoader.php
@@ -1,4 +1,5 @@
 <?php
+	require_once "authenticate.php";
 	function uploadsList() {
 		$files = scandir('uploads');
 		$files = array_diff($files, ['.', '..', '.git-keep']);
--- template.php
+++ template.php
@@ -1,3 +1,4 @@
+<?php require_once "authenticate.php"; ?>
 <!DOCTYPE html>
 <html>
 	<head>
