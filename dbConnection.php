d28a7c6f91cb3700d59f720e750f40d0ea6e1b87
qsus <106612195+qsus@users.noreply.github.com>
2024-04-03 20:27:25 +0200

    log exceptions

--- dbConnection.php
+++ dbConnection.php
@@ -3,8 +3,10 @@
 try {
 	// connect to database (and set error mode)
 	$pdo = new PDO("mysql:host=localhost;dbname=notes", 'notes', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-} catch (PDOException $e) {
+} catch (PDOException $e) { // connection failed
 	// error message
 	echo "Connecting to internal database failed.";
+	// log exception
+	file_put_contents('log/exception.log', $e->getMessage() . "\n", FILE_APPEND);
 	exit;
 }
