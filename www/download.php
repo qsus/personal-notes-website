f3c29a663ff4a58d1bf13a8170a0e7d58e31c348
qsus <106612195+qsus@users.noreply.github.com>
2024-04-11 21:10:04 +0200

    mod_xsendfile and header

--- www/download.php
+++ www/download.php
@@ -13,20 +13,36 @@ if (!isset($_GET['file'])) {
 	exit;
 }
 
-$file = basename($_GET['file']);
-$filename = __DIR__."/../uploads/$file";
-$downloadName = $_GET['name'] ?? $file;
+$fileName = basename($_GET['file']);
+$filePath = __DIR__."/../uploads/$fileName";
+$downloadName = $_GET['name'] ?? $fileName;
 $forceDownload = isset($_GET['download']) && $_GET['download'] != 'false';
 
-if (!file_exists($filename)) {
+if (!file_exists($filePath)) {
 	http_response_code(404);
 	exit;
 }
 
 header('Content-Disposition: '.($forceDownload ? 'attachment' : 'inline').'; filename="'.$downloadName.'"');
-header('Content-Type: '.mime_content_type($filename) ?? 'application/octet-stream');
-header('Content-Length: '.filesize($filename));
+header('Content-Type: '.mime_content_type($filePath) ?? 'application/octet-stream');
+header('Content-Length: '.filesize($filePath));
+header('Accept-Ranges: bytes');
+// allow caching
+header('Cache-Control: public');
+header('Pragma: public');
+// check hash of the cached file
+header('ETag: '.md5_file($filePath));
+if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == md5_file($filePath)) {
+	http_response_code(304);
+	exit;
+}
 
-// return the file and exit
-readfile($filename);
-exit;
+// send the file, using X-Sendfile if available
+if (function_exists('apache_get_modules') && in_array('mod_xsendfile', apache_get_modules())) {
+	header('X-Sendfile: '.$filePath);
+	exit;
+} else {
+	// return the file and exit
+	readfile($filePath);
+	exit;
+}
