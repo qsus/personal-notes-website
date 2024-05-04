39aac5f37e296b72135658f177e2ce2f6afafd97
qsus <106612195+qsus@users.noreply.github.com>
2024-05-04 12:20:40 +0200

    logout done by index

--- www/index.php
+++ www/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once __DIR__."/../scripts/authenticate.php";
 
+// if the request uri is logout.php, log the user out using logout.php
+if (strpos($_SERVER['REDIRECT_URL'], "/logout.php") === 0) {
+	require __DIR__."/../scripts/logout.php";
+	exit;
+}
+
 // if the request uri starts with /uploads/, serve the file using download.php
 if (strpos($_SERVER['REDIRECT_URL'], "/uploads/") === 0) {
 	require __DIR__."/../scripts/download.php";
