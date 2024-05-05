dddc3b48b4292514e2c113eb3fe52daef01f480b
qsus <106612195+qsus@users.noreply.github.com>
2024-05-05 12:30:17 +0200

    better url handling

--- www/index.php
+++ www/index.php
@@ -1,24 +1,48 @@
 <?php
-require_once __DIR__."/../scripts/authenticate.php";
+// get target from REQUEST_URI
+$target = explode("?", $_SERVER['REQUEST_URI'])[0];
+//$target = explode("/", $target)[1]; // remove leading slash
 
-// if the request uri is logout.php, log the user out using logout.php
-if (strpos($_SERVER['REDIRECT_URL'], "/logout.php") === 0) {
+// switch statement could be used, but it couldn't handle the /uploads/ folder
+// logout.php
+if (strpos($target, "/logout.php") === 0) {
 	require __DIR__."/../scripts/logout.php";
 	exit;
 }
 
-// if the request uri starts with /uploads/, serve the file using download.php
-if (strpos($_SERVER['REDIRECT_URL'], "/uploads/") === 0) {
+// files from /uploads/
+if (strpos($target, "/uploads/") === 0) {
+	require_once __DIR__."/../scripts/authenticate.php";
 	require __DIR__."/../scripts/download.php";
 	exit;
 }
 
-// if the request uri starts with upload.php, handle the upload
-if (strpos($_SERVER['REDIRECT_URL'], "/upload.php") === 0) {
+// upload.php
+if (strpos($target, "/upload.php") === 0) {
+	require_once __DIR__."/../scripts/authenticate.php";
 	require __DIR__."/../scripts/upload.php";
 	exit;
 }
 
-require_once __DIR__."/../scripts/fileLoader.php";
-$uploads = uploadsList();
-require __DIR__."/../templates/index.php";
+// index.php
+if (strpos($target, "/index.php") === 0) {
+	// not requiring authentication
+	// redirect to /
+	http_response_code(301);
+	header("Location: /");
+	exit;
+}
+
+// /
+if ($target === "/") {
+	require_once __DIR__."/../scripts/authenticate.php";
+	require __DIR__."/../scripts/fileLoader.php";
+	$uploads = uploadsList();
+	require __DIR__."/../templates/index.php";
+	exit;
+}
+
+// default
+http_response_code(404);
+header("Location: /");
+exit;
