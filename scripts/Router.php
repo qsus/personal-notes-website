fab8e4f779c17c1d26c738b252c995d87cba027d
qsus <106612195+qsus@users.noreply.github.com>
2024-05-28 15:29:32 +0200

    replace splitting URL segments in router with regex

--- scripts/Router.php
+++ scripts/Router.php
@@ -6,19 +6,33 @@ namespace App;
 
 class Router
 {
+    private array $routes = [];
+
+    public function __construct()
+    {
+        // regex pattern to match uri
+        $this->routes = [
+            // note that dollar sign ensures that the uri ends with the pattern
+            '/^$/' => "IndexController",
+            '/^logout$/' => "LogoutController",
+            '/^uploads/' => "DownloadController",
+            '/^download/' => "DownloadController",
+            '/^upload/' => "UploadController",
+        ];
+    }
+
     public function resolve(string $uri): string // /a/b/c?d=e&f=g
     {
         // convert uri to array of names between slashes
         $uri = explode('?', $uri)[0];            // /a/b/c
         $uri = trim($uri, '/');                  // a/b/c
-        $uri = explode('/', $uri);               // ['a', 'b', 'c']
 
-        // resolve controller name
-        if ($uri == ['']) return "IndexController";
-        if ($uri == ['logout']) return "LogoutController";
-        if ($uri[0] == 'uploads') return "DownloadController";
-        if ($uri[0] == 'download') return "DownloadController";
-        if ($uri[0] == 'upload') return "UploadController";
+        // loop through routes and return controller name if uri matches
+        foreach ($this->routes as $pattern => $controllerName) {
+            if (preg_match($pattern, $uri)) {
+                return $controllerName;
+            }
+        }
         
         // unresolvable uri
         return "NotFoundController";
